4062b2666cde1379571eebd4cbae1f884084e6b3
Lucasacademico <100856355+Lucasacademico@users.noreply.github.com>
2023-09-26 01:34:39 +0000

    atualizado estrutura map para itens de card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import './App.css' 
-
-
+import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
+
+  const item1 = {
+    nome: 'Rick Sanchez',
+    imageUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    nome: 'Morty Smith',
+    imageUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    nome: 'Summer Smith',
+    imageUrl: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <div className="card">
-        <h2 class="pointer">Rick Sanchez</h2>
-
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
-
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      {itens.map(function (item) {
+        return <Card item={item}/>
+      })}
     </>
   )
 }
 
 export default App
-// 
